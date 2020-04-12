98b0864a8699d11e0c57de86dfbd678b5bc273d2
Adam Arthur Faizal <adam.faizal.af6@gmail.com>
2020-04-12 10:13:03 +0700

    tugas 5 sama 6 lupa tak export wkwk

---
+++ Tugas/tugas5-6.sql
@@ -0,0 +1,214 @@
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: Tugas2BasisData
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `barang`
+--
+
+DROP TABLE IF EXISTS `barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `barang` (
+  `id_barang` int(10) NOT NULL AUTO_INCREMENT,
+  `id_jenis` int(10) NOT NULL,
+  `id_merk` int(10) NOT NULL,
+  `harga` int(10) NOT NULL,
+  PRIMARY KEY (`id_barang`),
+  KEY `id_jenis` (`id_jenis`),
+  KEY `id_merk` (`id_merk`),
+  CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_jenis`) REFERENCES `jenis_barang` (`id_jenis`),
+  CONSTRAINT `barang_ibfk_2` FOREIGN KEY (`id_merk`) REFERENCES `merk_barang` (`id_merk`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `barang`
+--
+
+LOCK TABLES `barang` WRITE;
+/*!40000 ALTER TABLE `barang` DISABLE KEYS */;
+INSERT INTO `barang` VALUES (6,1,1,10000),(7,1,2,10000),(8,1,3,10000);
+/*!40000 ALTER TABLE `barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detail_pembelian`
+--
+
+DROP TABLE IF EXISTS `detail_pembelian`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detail_pembelian` (
+  `id_detail` int(10) NOT NULL AUTO_INCREMENT,
+  `id_pembelian` int(10) NOT NULL,
+  `id_barang` int(10) NOT NULL,
+  PRIMARY KEY (`id_detail`),
+  KEY `id_pembelian` (`id_pembelian`),
+  KEY `id_barang` (`id_barang`),
+  CONSTRAINT `detail_pembelian_ibfk_1` FOREIGN KEY (`id_pembelian`) REFERENCES `pembelian` (`id_pembelian`),
+  CONSTRAINT `detail_pembelian_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detail_pembelian`
+--
+
+LOCK TABLES `detail_pembelian` WRITE;
+/*!40000 ALTER TABLE `detail_pembelian` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detail_pembelian` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jenis_barang`
+--
+
+DROP TABLE IF EXISTS `jenis_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jenis_barang` (
+  `id_jenis` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_jenis` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_jenis`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jenis_barang`
+--
+
+LOCK TABLES `jenis_barang` WRITE;
+/*!40000 ALTER TABLE `jenis_barang` DISABLE KEYS */;
+INSERT INTO `jenis_barang` VALUES (1,'Sembako'),(2,'Elektronik'),(3,'Pakaian');
+/*!40000 ALTER TABLE `jenis_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kota`
+--
+
+DROP TABLE IF EXISTS `kota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kota` (
+  `id_kota` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_kota` varchar(30) NOT NULL,
+  `besar_ongkir` int(10) NOT NULL,
+  PRIMARY KEY (`id_kota`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kota`
+--
+
+LOCK TABLES `kota` WRITE;
+/*!40000 ALTER TABLE `kota` DISABLE KEYS */;
+INSERT INTO `kota` VALUES (1,'Surakarta',10000),(2,'Karanganyar',12000),(3,'Sragen',0);
+/*!40000 ALTER TABLE `kota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `merk_barang`
+--
+
+DROP TABLE IF EXISTS `merk_barang`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `merk_barang` (
+  `id_merk` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_merk` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_merk`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `merk_barang`
+--
+
+LOCK TABLES `merk_barang` WRITE;
+/*!40000 ALTER TABLE `merk_barang` DISABLE KEYS */;
+INSERT INTO `merk_barang` VALUES (1,'Gudang Garam'),(2,'Gudang Gula'),(3,'Gudang Beras');
+/*!40000 ALTER TABLE `merk_barang` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pelanggan`
+--
+
+DROP TABLE IF EXISTS `pelanggan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(10) NOT NULL AUTO_INCREMENT,
+  `nama_pelanggan` varchar(15) NOT NULL,
+  `no_hp` int(15) NOT NULL,
+  `email` varchar(15) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `jenis_kelamin` enum('L','P') NOT NULL,
+  `id_kota` int(10) NOT NULL,
+  PRIMARY KEY (`id_pelanggan`),
+  KEY `id_kota` (`id_kota`),
+  CONSTRAINT `pelanggan_ibfk_1` FOREIGN KEY (`id_kota`) REFERENCES `kota` (`id_kota`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+LOCK TABLES `pelanggan` WRITE;
+/*!40000 ALTER TABLE `pelanggan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pelanggan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pembelian`
+--
+
+DROP TABLE IF EXISTS `pembelian`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pembelian` (
+  `id_pembelian` int(10) NOT NULL AUTO_INCREMENT,
+  `tanggal` date NOT NULL,
+  `id_pelanggan` int(10) NOT NULL,
+  PRIMARY KEY (`id_pembelian`),
+  KEY `id_pelanggan` (`id_pelanggan`),
+  CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pembelian`
+--
+
+LOCK TABLES `pembelian` WRITE;
+/*!40000 ALTER TABLE `pembelian` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pembelian` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-12 10:12:14
